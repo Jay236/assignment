0062585235f7a90eb4411d0b937a6f55a7c4c9fa
Jay236 <jaychander39@gmail.com>
2017-08-14 15:54:17 -0400

    js file

---
+++ demo.js
@@ -0,0 +1,15 @@
+    function pagetst0(){
+        window.location.href ="http://127.0.0.1:52359/demo0.html";
+    }
+    
+function pagetst1() 
+{
+    window.location.href = "http://127.0.0.1:52359/demo1.html";
+}
+    function pagetst2(){
+        window.location.href ="http://127.0.0.1:52359/demo2.html";
+    }
+    function pagetst3(){
+        window.location.href ="http://127.0.0.1:52359/demo3.html";
+    }
+    
